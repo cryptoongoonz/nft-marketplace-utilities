523e0351681a0a60f07e970831f1dc1e76fdef35
Jesse Friedland <jesse.friedland@gmail.com>
2022-03-06 09:57:40 -0500

    Organizing nft marketplace tools

---
+++ listing-history/listing-history-opensea.sh
@@ -0,0 +1,5 @@
+for i in {1..6969}; do
+	printf "${i},"
+	curl -s "https://api.opensea.io/wyvern/v1/orders?asset_contract_address=0x0322f6f11a94cfb1b5b6e95e059d8deb2bf17d6a&bundled=false&include_bundled=false&token_id=${i}&side=1&limit=20&offset=0&order_by=created_date&order_direction=desc" --header 'Accept: application/json' --header 'X-API-KEY: ***REMOVED***' | jq -r '.orders[0] | "\(.maker.address),\(.created_date),\(.cancelled)"'
+	sleep 1;
+done
